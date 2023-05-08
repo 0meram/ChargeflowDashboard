634f7b9f93920662aeb1a3d218166ad944a55451
omer ram <0meram95@gmail.com>
2023-05-08 11:36:40 +0300

    base layout

--- src/App.js
+++ src/App.js
@@ -10,11 +10,8 @@ const App = () => {
 
   return (
     <div className={`grid-container ${asideActive ? 'aside-active' : ''}`}>
-      <div className="menu-icon" onClick={toggleAside}>
-        <strong> &#9776;</strong>
-      </div>
       <header className="header">
-        <div className="header_search"> icon Search</div>
+        <div className="header_search">icon Search</div>
         <div className="header_icons">
           <div className="header_icon">icon</div>
           <div className="header_icon">icon</div>
@@ -22,41 +19,36 @@ const App = () => {
         </div>
       </header>
       <aside className={`aside ${asideActive ? 'active' : ''}`}>
-        <div className="aside_close-icon" onClick={toggleAside}>
-          <strong>&times;</strong>
-          <div>hwloooooooooo</div>
-        </div>
+        <h2 className="aside_title">store name</h2>
         <ul className="aside_list">
-          <li className="aside_list-item">Menu item1</li>
-          <li className="aside_list-item">Menu item2</li>
-          <li className="aside_list-item">Menu item3</li>
-          <li className="aside_list-item">Menu item4</li>
-          <li className="aside_list-item">Menu item5</li>
+          <li><icon>icon</icon>Payments</li>
+          <li><icon>icon</icon>Balances</li>
+          <li><icon>icon</icon>Customers</li>
+          <li><icon>icon</icon>Products</li>
+          <li><icon>icon</icon>Reports</li>
+          <br></br>
+          <li><icon>icon</icon>Developers</li>
+          <li><icon>icon</icon>View test data</li>
+          <li><icon>icon</icon>Settings
+          </li>
         </ul>
       </aside>
       <main className="main">
-        <div className="main_overview">
-          <div className="overview_card">
-            <div className="overview_card-info">Overview</div>
-            <div className="overview_card-icon">Card</div>
-          </div>
-          <div className="overview_card">
-            <div className="overview_card-info">Overview</div>
-            <div className="overview_card-icon">Card</div>
-          </div>
-          <div className="overview_card">
-            <div className="overview_card-info">Overview</div>
-            <div className="overview_card-icon">Card</div>
-          </div>
-          <div className="overview_card">
-            <div className="overview_card-info">Overview</div>
-            <div className="overview_card-icon">Card</div>
-          </div>
+        <h1 className='main_title'>Today</h1>
+        <div className="charts">
+          <div className="main_chart">Chart</div>
+          <div className="chart1">Card</div>
+          <div className="chart2">Card</div>
         </div>
-        <div className="main_cards">
-          <div className="card">Card</div>
-          <div className="card">Card</div>
-          <div className="card">Card</div>
+        <h1 className='main_title'>Reports overview</h1>
+        <div>filters</div>
+        <div className="reports">
+          <div className="card1">Chart</div>
+          <div className="card2">Card</div>
+          <div className="card3">Card</div>
+          <div className="card4">Card</div>
+          <div className="card5">Card</div>
+          <div className="card6">Card</div>
         </div>
       </main>
     </div>
