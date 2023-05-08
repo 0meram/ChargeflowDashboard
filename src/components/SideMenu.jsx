9e5d46ca5bdfe703bde1b67d1ac0d260af77f368
omer ram <0meram95@gmail.com>
2023-05-08 14:25:53 +0300

    side menu wip

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as Payment } from '../icons/payments.svg';
 import { ReactComponent as Balance } from '../icons/balance.svg';
 import { ReactComponent as Product } from '../icons/product.svg';
@@ -9,24 +9,28 @@ import { ReactComponent as Account } from '../icons/acounts.svg';
 import { ReactComponent as Report } from '../icons/report.svg';
 import { ReactComponent as Developer } from '../icons/developer.svg';
 import { ReactComponent as Costumer } from '../icons/costumers.svg';
+import { ReactComponent as Arrow } from '../icons/arrow.svg';
+import { ReactComponent as Home } from '../icons/home.svg';
 
 const SideMenu = () => {
     const [asideActive, setAsideActive] = useState(false);
 
     return (
         <aside className={`aside ${asideActive ? 'active' : ''}`}>
-            <h2 className="aside_title"><div><Store /></div>store name</h2>
+            <h2 className="aside_title"><div><Store /></div>store name<Arrow /></h2>
             <ul className="aside_list">
-                <li><Payment />Payments</li>
-                <li><Balance />Balances</li>
-                <li><Costumer />Customers</li>
-                <li><Product />Products</li>
-                <li><Report />Reports</li>
-                <br></br>
-                <li><Payment />Developers</li>
-                <li><Toggle />View test data</li>
-                <li><Settings />Settings
-                </li>
+                <div><Home /><li>Home</li></div>
+                <br />
+                <div><Payment /><li>Payments</li></div>
+                <div><Balance /><li>Balances</li></div>
+                <div><Costumer /><li>Customers</li></div>
+                <div><Account /><li>Connected accounts</li></div>
+                <div><Product /><li>Products</li></div>
+                <div><Report /><li>Reports</li></div>
+                <br />
+                <div><Payment /><li>Developers</li></div>
+                <div><Toggle /><li>View test data</li></div>
+                <div><Settings /><li>Settings</li></div>
             </ul>
         </aside>
     )
